fe54ab916c872c010005e95183c343420e98cce3
Aya Elalfy <ayaelalfy639@gmail.com>
2026-04-18 13:35:45 +0200

    edit the repo to get the photo

--- api/auth_api.py
+++ api/auth_api.py
@@ -13,14 +13,14 @@ router = APIRouter(prefix="/auth", tags=["Auth"])
 @router.post("/google/login", response_model=TokenResponse)
 def google_login(request: GoogleAuthRequest, db: Session = Depends(get_db)):
     
-    # 🔥 1. exchange code → token
+    # 1. exchange code → token
     token_data = exchange_code_for_token(request.code)
 
     access_token = token_data.get("access_token")
     if not access_token:
         raise HTTPException(status_code=400, detail="Failed to exchange code for token")
 
-    # 🔥 2. get user info
+    #  2. get user info
     user_info = get_user_info(access_token)
 
     email = user_info["email"]
@@ -30,14 +30,20 @@ def google_login(request: GoogleAuthRequest, db: Session = Depends(get_db)):
 
     name = user_info.get("name")
     google_id = user_info["id"]
+    photo = user_info.get("picture")
 
-    # 🔥 3. check user
+    # 3. check user
     user = get_user_by_email(db, email)
-
     if not user:
-        user = create_user(db, email, name, google_id)
+        user = create_user(db, email, name, google_id, photo)  
+    else:
+        # ← لو موجود حدّث الـ photo لو اتغيرت
+        user.photo = photo
+        db.commit()
+        db.refresh(user)
 
-    # 🔥 4. create JWT
+
+    #  4. create JWT
     jwt_token = create_access_token({
         "user_id": str(user.id),
         "role": user.role,
--- repositories/user_repository.py
+++ repositories/user_repository.py
@@ -4,11 +4,12 @@ from models.user import User
 def get_user_by_email(db: Session, email: str):
     return db.query(User).filter(User.email == email).first()
 
-def create_user(db: Session, email: str, name: str, google_id: str):
+def create_user(db: Session, email: str, name: str, google_id: str ,photo: str = None):
     user = User(
         email=email,
         name=name,
-        google_id=google_id
+        google_id=google_id,
+        photo=photo 
     )
     db.add(user)
     db.commit()
